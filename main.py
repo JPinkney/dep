b9e6628c6e2ef10005467a51a914dd6b49a6761d
Josh Pinkney <Joshpinkney@gmail.com>
2017-07-08 18:16:33 -0400

    Added initial version of code. Currently tested on OSX

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import os
+import platform
 import json
+import os
+import subprocess
 
 MAC_OS = "OSX"
 LINUX_OS = "Linux"
 WINDOWS_OS = "Windows"
+OTHER_OS = "Other"
 
-COMMAND_LIST = [MAC_OS, LINUX_OS, WINDOWS_OS]
+COMMAND_LIST = [MAC_OS, LINUX_OS, WINDOWS_OS, OTHER_OS]
 json_output = {}
 
 def setup_file():
@@ -30,20 +33,48 @@ def setup_file():
 					dependency = split_line[1].strip()
 					json_output[last_cmd].append({
 						"packagemanager": package_manager,
-						"dependency": dependency
+						"dependency": dependency,
+						"install": input_token
 					})
-
-		print(json.dumps(json_output, indent=4))
+		return json_output
+		
 
 def check_for_header(input_str):
 	for command in COMMAND_LIST:
-		if input_str == command:
+		if input_str.lower() == command.lower():
 			return True
 	return False
 
-def install_dependencies():
-	pass
+def install_dependencies(depencencies_for_os):
+	for install_item in depencencies_for_os:
+		if is_package_manager_found(install_item["packagemanager"]):
+			install_dependency(install_item["install"])
+
+def is_package_manager_found(package_manager):
+	try:
+		p = subprocess.Popen(package_manager, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	except:
+		print("Could not find dependency manager: " + package_manager)
+		return False
+	return True
+
+def install_dependency(depencency):
+	try:
+		print("Installing dependency: " + depencency)
+		p = subprocess.Popen(depencency, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+		out, err = p.communicate()
+		print(out)
+		print(err)
+		print("Installed dependency: " + depencency)
+	except:
+		print("Installing dependency: " + depencency + " failed")
 
 
 if __name__ == '__main__':
-	setup_file()
+	current_os = platform.system()
+	if current_os == "Darwin":
+		current_os = "OSX"
+	depencencies_json = setup_file()
+	depencencies_for_os = depencencies_json[current_os]
+	print("Installing depencies for " + current_os)
+	install_dependencies(depencencies_for_os)
